81a56b6372dd9eb8df4763c9f165e2902ed9595d
Annie Spahn <soccercat11@hotmail.com>
2016-06-23 13:44:54 -0600

    Basic deck working

---
+++ deck.py
@@ -0,0 +1,66 @@
+import sys
+import random
+
+class Deck(object):
+
+    CLUB =u'\u2660'
+    HEART =u'\u2665'
+    DIAMOND =u'\u2666'
+    SPADE =u'\u2663'
+    V =u'\u00A6'
+
+    # Abstract
+
+    #interface
+    #print_card
+    #shuffle
+    #card_value
+    #card_suit
+    pass
+
+class BasicDeck(Deck):
+
+    def __init__(self, num_single_decks):
+        self.deck = [" "] * (52*num_single_decks)
+
+        for d in range(num_single_decks):
+            for i in range(13):
+                self.deck[i + 52*d] = 'C' + str(i+1)
+                self.deck[i+13 + 52*d] = 'H' + str(i+1)
+                self.deck[i+26 + 52*d] = 'D' + str(i+1)
+                self.deck[i+39 + 52*d] = 'S' + str(i+1)
+
+    def print_card(self, card_idx):
+        x = self.card_suit(card_idx)
+
+        print("-------")
+        print(Deck.V+ "%d    " + Deck.V) %(self.card_value(card_idx))
+        print(Deck.V+ "      " + Deck.V)
+        print(Deck.V+ "  " +  x  + "   "  + Deck.V)
+        print(Deck.V+ "      " + Deck.V)
+        print(Deck.V+ "    %d" + Deck.V) %(self.card_value(card_idx))
+        print("--------")
+
+    def card_value(self, card_idx):
+        return int(self.deck[card_idx][1:])
+
+    def card_suit(self, card_idx):
+        if self.deck[card_idx][0] == 'C':
+            return Deck.CLUB
+        elif self.deck[card_idx][0] == 'H':
+            return Deck.HEART
+        elif self.deck[card_idx][0] == 'D':
+            return Deck.DIAMOND
+        elif self.deck[card_idx][0] == 'S':
+            return Deck.SPADE
+        else:
+            assert False, "Bad suit %s" % self.deck[card_idx][0]
+
+    def main(self):
+       print(self.club)
+       print(self.heart)
+       print(self.diamond)
+       print(self.spade)
+
+d=BasicDeck(int(sys.argv[1]))
+d.print_card(2)
