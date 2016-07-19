c9924d99c83231706ee83f3bfea0dcc680450a33
Annie Spahn <soccercat11@hotmail.com>
2016-07-19 08:50:59 -0600

    correct player betting

--- blackjack.py
+++ blackjack.py
@@ -26,14 +26,12 @@ class BlackJack():
             return 0
         elif self.the_dealer.player_score(num_decks) == 21 and len(self.the_dealer.total_player) == 2:
             return 2
-        elif (21 - self.the_dealer.dealer_score(num_decks)) == (21 - self.the_dealer.player_score(num_decks)):
-            return 0
-        elif self.the_dealer.player_score(num_decks) == 21:
-            return 1
-        elif (21 - self.the_dealer.dealer_score(num_decks)) < 0:
-            return 1
         elif (21 - self.the_dealer.player_score(num_decks)) < 0:
             return -1
+        elif (21 - self.the_dealer.dealer_score(num_decks)) < 0:
+            return 1
+        elif (21 - self.the_dealer.dealer_score(num_decks)) == (21 - self.the_dealer.player_score(num_decks)):
+            return 0
         elif (21 - self.the_dealer.dealer_score(num_decks)) < (21 - self.the_dealer.player_score(num_decks)):
             return -1
         elif (21 - self.the_dealer.dealer_score(num_decks)) > (21 - self.the_dealer.player_score(num_decks)):
@@ -42,8 +40,9 @@ class BlackJack():
             pass
 
     def play(self):
-        'Turn off output'
+        'uncomment to turn off output'
         sys.stdout = open('/dev/null', 'w')
+        sys.stdout = open('nul', 'w')
 
         print('')
         print("     "'*' * 5 + " Welcome to Blackjack!" + "     "'*' * 5)
@@ -52,15 +51,15 @@ class BlackJack():
         print(" " * 10 + "The game is very simple, but here are a few rules to get you started: ")
         print('')
         print(
-        "The goal is to closer to 21 than the dealer. \nYou will be dealt two cards and then asked if you want to hit(get another card) or if you want to stand (stay with the cards you have). \nThe dealer than will take cards until his score is at least 17. Whoever is closer to 21 wins! \nBut be careful if you go over 21, you lose!")
+            "The goal is to closer to 21 than the dealer. \nYou will be dealt two cards and then asked if you want to hit(get another card) or if you want to stand (stay with the cards you have). \nThe dealer than will take cards until his score is at least 17. Whoever is closer to 21 wins! \nBut be careful if you go over 21, you lose!")
         print('')
         print(
-        "You will start with $100,000 in you bank and your winnings and bets will be subtracted or added to it. \nGood luck!")
+            "You will start with $100,000 in you bank and your winnings and bets will be subtracted or added to it. \nGood luck!")
         print('')
         num_hands = 0
         auto = True
         self.the_dealer.the_deck.shuffle_deck()
-        while self.the_player.bank > 0 and num_hands < 10:
+        while self.the_player.bank > 0 and num_hands < 5000:
             self.the_dealer.new_hand()
             print('')
             print("This is currently how much money you have in your bank account: " + str(self.the_player.bank))
@@ -73,22 +72,23 @@ class BlackJack():
                 self.the_dealer.dealer_card(self.num_decks)
                 self.the_turn += 1
             if self.winner(self.num_decks) == 1:
-                print("Congrats Player! You have won.")
+                print("Congrats Player! You have won: " + str(self.the_player.bet))
                 self.num_win += 1
-                self.the_player.bank += self.the_player.bet * 2
+                self.the_player.bank += self.the_player.bet
                 self.player_winnings += self.the_player.bet
             elif self.winner(self.num_decks) == 2:
-                print("Congrats you have gotten blackjack!")
+                print("Congrats you have gotten blackjack! and won: " + str(self.the_player.bet * 1.5))
                 self.num_win += 1
-                self.the_player.bank += self.the_player.bet * (2.5)
+                self.the_player.bank += self.the_player.bet * (1.5)
                 self.player_winnings += self.the_player.bet * (1.5)
             elif self.winner(self.num_decks) == -1:
-                print("Sorry Player. You have lost.")
+                print("Sorry Player. You have lost: " + str(self.the_player.bet))
                 self.num_lose += 1
+                self.the_player.bank -= self.the_player.bet
                 self.player_losings += self.the_player.bet
             else:
                 print("The game has ended in a tie")
-                self.the_player.bank += self.the_player.bet
+                #self.the_player.bank += self.the_player.bet
                 self.num_tie += 1
             self.display_player_money()
             print('')
@@ -114,12 +114,11 @@ class BlackJack():
     def leave_game(self):
         sys.stdout = self.stdoutTemp
         x = self.the_dealer.deck_shuffles
-        print('')*2
         print("This is how many times the deck has been reshuffled: " + str(x))
-        print("This is the total number of  games you  won: " + str(self.num_win))
-        print("This is the total number of  games you lost: " + str(self.num_lose))
-        print("This is the total number of games  you tied: " + str(self.num_tie))
-        print("This is how much money you have left in your bank: " + str(self.the_player.bank))
+        print("This is how many games you have won: " + str(self.num_win))
+        print("This is how many games you lost: " + str(self.num_lose))
+        print("This is how many games you tied: " + str(self.num_tie))
+        print("This is how much money you have in your bank: " + str(self.the_player.bank))
         sys.exit()
 
     def display_player_money(self):
@@ -130,5 +129,5 @@ class BlackJack():
         print("This is how much money you have lost: " + str(self.player_losings))
 
 
-b = BlackJack(6, 25, 250)
+b = BlackJack(1, 25, 250)
 b.play()
